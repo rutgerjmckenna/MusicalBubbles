c367cc05db61ff03ea6895cf861b064401ea740d
Rutger McKenna <44029087+rutgerjmckenna@users.noreply.github.com>
2020-05-04 20:44:25 -0400

    first circle

--- View.js
+++ View.js
@@ -8,4 +8,13 @@ View.prototype.updateDisplay = function() {
     context.clearRect(0, 0, view.canvas.width, view.canvas.height);
     context.fillStyle = 'black';
     context.fillRect(0, 0, view.canvas.width, view.canvas.height);
+
+    view.drawCircle(context, 150, 150, 100, 1);
+};
+
+View.prototype.drawCircle = function(context, x, y, radius, alpha) {
+    context.beginPath();
+    context.arc(x, y, radius, 0, 2*Math.PI);
+    context.fillStyle = "rgba(" + x%256 + ", " + y%256  + ", " + (x * y % 256)+ " ," + alpha + ")";
+    context.fill();
 }
